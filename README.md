689258447a24a59e84f43d69b9318e17aaccda61
Vincent Hamp <higaski@users.noreply.github.com>
2024-08-04 13:49:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# openremise.github.io
---
+++ src/doxygen.hpp
@@ -0,0 +1,8 @@
+/// Documentation
+///
+/// \file   doxygen.hpp
+/// \author Vincent Hamp
+/// \date   04/08/2024
+
+/// \mainpage Introduction
+/// Hello there! This is a source file used for documentation :)
