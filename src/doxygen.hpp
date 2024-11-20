84457bf725f4deb06dc4a90397ffd5ba033f3949
Vincent Hamp <vincent.hamp@higaski.at>
2024-11-20 20:17:46 +0100

    feat: started with introduction

--- src/doxygen.hpp
+++ src/doxygen.hpp
@@ -20,33 +20,79 @@
 /// \date   04/08/2024
 
 /// \mainpage Introduction
-/// Hello there! This is a source file used for documentation :)
+/// \details \tableofcontents
+/// Welcome to the [OpenRemise](https://openremise.at) project.
 ///
-/// Features
-/// - Open source hard- & software
+/// \section section_features Features
+/// <div class="tabbed">
+/// - <b class="tab-title">Available</b>
+///   - \image{inline} html Open_Source_Initiative.svg width=16px &nbsp;Open
+///   source hard- & software
+///   - \image{inline} html square_wave.svg width=16px &nbsp;DCC
+///   - \htmlonly &#9107; \endhtmlonly Adjustable current limits (0.5, 1.3, 2.7
+///   and 4.1A)
+///   - \htmlonly &#128268; \endhtmlonly Plug&Play (no app)
+///   - \image{inline} html 3rd_party.svg width=16px &nbsp;3rd party support
+///   (JMRI, Z21, ZIMO ZSP, ...)
+/// - <b class="tab-title">Planned</b>
+///   - \emoji :de: German translation of the frontend
+///   - \image{inline} html network.svg width=16px &nbsp;Bus systems (XPressNet,
+///   LocoNet, LCC, BiDiB)
+/// - <b class="tab-title">Not planned</b>
+///   - \image{inline} html sine_wave.svg width=16px &nbsp;Analog operation
+///   - \image{inline} html square_wave.svg width=16px &nbsp;Other track
+///   protocols
+/// </div>
+/// <div class="section_buttons">
+/// | Next                      |
+/// | ------------------------: |
+/// | \ref page_getting_started |
+/// </div>
+
+/// \page page_introduction2 Introduction 2
+/// \details \tableofcontents
+/// \section section_features2 Features
+/// - \image{inline} html Open_Source_Initiative.svg width=16px &nbsp;Open
+/// source hard- & software
+/// - \image{inline} html square_wave.svg width=16px &nbsp;DCC
+/// - \htmlonly &#9107; \endhtmlonly Adjustable current limits
+/// (0.5, 1.3, 2.7 and 4.1A)
+/// - \htmlonly &#128268; \endhtmlonly Plug&Play (no app)
 ///
-/// - Plug&Play
-///   - No app
+/// - \image{inline} html 3rd_party.svg width=16px &nbsp;3rd party support
+/// (JMRI, Z21, ZIMO ZSP, ...)
 ///
-/// - 3rd party support
-///   - JMRI
-///   - Z21 app
-///   - ZIMO ZSP
+/// \subsection section_planned Planned
+/// - \emoji :de: German translation of the frontend
+/// - \image{inline} html network.svg width=16px &nbsp;Bus systems (XPressNet,
+/// LocoNet, LCC, BiDiB)
 ///
-/// Planned
-/// - Bussystem (XPressNet, LocoNet, LCC, BiDiB)
+/// \subsection section_not_planned Not planned
+/// - \image{inline} html sine_wave.svg width=16px &nbsp;Analog operation
+/// - \image{inline} html square_wave.svg width=16px &nbsp;Other track protocols
+/// besides DCC
 ///
-/// Not planned
-/// - Other track formats
-///
+/// <div class="section_buttons">
+/// | Next                      |
+/// | ------------------------: |
+/// | \ref page_getting_started |
+/// </div>
+
 /// \page page_getting_started Getting Started
 /// \details \tableofcontents
 /// \todo Where to get the hardware, how to install using the Flasher
 ///
 /// \section subpage_hardware Hardware
-/// Bla
-///
+/// Blar
+
 /// \page page_support Support
+/// \details \tableofcontents
 /// \todo Where to get support? I guess GitHub issues mostly, maybe email?
 ///
 ///
+/// | Current [A] | Nominal size                                       |
+/// | ----------- | -------------------------------------------------- |
+/// | 0.5         | Service mode                                       |
+/// | 1.3         | [Z scale](https://en.wikipedia.org/wiki/Z_scale)   |
+/// | 2.7         | [N scale](https://en.wikipedia.org/wiki/N_scale)   |
+/// | 4.1         | [H0 scale](https://en.wikipedia.org/wiki/HO_scale) |
