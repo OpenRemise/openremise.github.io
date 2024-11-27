49f636caceffe38eb2ab00e90e53a3a245fa5994
Vincent Hamp <vincent.hamp@higaski.at>
2024-11-27 18:26:37 +0100

    feat: more history, some FAQ

--- src/doxygen.hpp
+++ src/doxygen.hpp
@@ -38,12 +38,13 @@
 /// <div class="tabbed">
 /// - <b class="tab-title">Available</b>
 ///   \image{inline} html open_source_initiative.svg width=16px
-///   <b>Open source hard- & software</b><br>
+///   **Open source hard- & software**<br>
 ///   Both the hard- and the software are freely available under copyleft
 ///   licenses. They do not contain any proprietary bits and everyone is free to
 ///   use or modify it as they see fit.
 ///   <br><br>
-///   \htmlonly &#128268; \endhtmlonly <b>Plug&Play</b><br>
+///   \htmlonly &#128268; \endhtmlonly
+///   **Plug&Play**<br>
 ///   The entire system is almost plug and play. Only at the beginning does the
 ///   firmware have to be flashed onto the board. A [user-friendly tool for
 ///   Windows and Linux](https://github.com/OpenRemise/Flasher) is available
@@ -53,28 +54,29 @@
 ///   to try it out.
 ///   <br><br>
 ///   \image{inline} html square_wave.svg width=16px
-///   <b>DCC</b><br>
+///   **DCC**<br>
 ///   DCC is the only track protocol supported, but in the most compatible yet
 ///   still cutting edge way. Changes to the standards are adopted quickly while
 ///   maintaining backwards compatibility.
 ///   <br><br>
-///   \htmlonly &#9107; \endhtmlonly <b>Adjustable current limits</b><br>
+///   \htmlonly &#9107; \endhtmlonly
+///   **Adjustable current limits**<br>
 ///   4 adjustable current limits (0.5, 1.3, 2.7 and 4.1A) ensure operational
 ///   safety. The lowest limit is also implicitly applied in service mode.
 ///   <br><br>
 ///   \image{inline} html system_update_alt.svg width=16px
-///   <b>Decoder updates</b><br>
+///   **Decoder updates**<br>
 ///   All [ZIMO](https://www.zimo.at/web2010/index.html) MS-, MN- and MX
 ///   decoders can be updated.
 ///   <br><br>
 ///   \emoji :musical_note:
-///   <b>Sound</b><br>
+///   **Sound**<br>
 ///   All [ZIMO](https://www.zimo.at/web2010/index.html) MS-, MN- and MX
 ///   decoders can be loaded with sound (or general projects).
 ///   <br><br>
 ///   \image{inline} html 3rd_party.svg width=16px
-///   <b>3rd party
-///   support</b><br>
+///   **3rd party
+///   support**<br>
 ///   The WiFi and USB interfaces allow a variety of 3rd party support. For
 ///   example, OpenRemise can be used with the following apps
 ///   <ul>
@@ -98,49 +100,51 @@
 ///   <br>
 /// - <b class="tab-title">Planned</b>
 ///   \image{inline} html square_wave.svg width=16px
-///   <b>DCC</b><br>
+///   **DCC**<br>
 ///   DCC support isn't complete yet. First of all, accessory commands are not
 ///   yet implemented.
 ///   <br><br>
 ///   \image{inline} html system_update_alt.svg width=16px
-///   <b>Decoder updates</b><br>
+///   **Decoder updates**<br>
 ///   [tams elektronik](https://tams-online.de) has already provided information
 ///   on their decoder update procedure. An implementation in the near future is
 ///   very likely.
 ///   <br><br>
 ///   \image{inline} html network.svg width=16px
-///   <b>Bus systems</b><br>
+///   **Bus systems**<br>
 ///   Sooner or later, a fully-fledged command station will need to be connected
 ///   to existing bus systems in order to integrate handheld controllers or
 ///   section modules. It is currently not clear which buses will be supported,
 ///   but XPressNet, LocoNet, LCC and BiDiB would be possible. Feedback on this
 ///   is welcome.
 ///   <br><br>
-///   \emoji :de: <b>German translation of the frontend</b><br>
+///   \emoji :de:
+///   **German translation of the frontend**<br>
 ///   The [Frontend](https://openremise.at/Frontend) is currently only available
 ///   in English. As a native German speaker, this is a bit embarrassing...
 ///   <br><br>
 /// - <b class="tab-title">Not planned</b>
 ///   \image{inline} html square_wave.svg width=16px
-///   <b>Other track protocols</b><br>
+///   **Other track protocols**<br>
 ///   Contributions are welcome, but I will certainly not implement a different
 ///   other track protocol.
 ///   <br><br>
 ///   \image{inline} html system_update_alt.svg width=16px
-///   <b>Decoder updates</b><br>
+///   **Decoder updates**<br>
 ///   [Doehler & Haass](https://doehler-haass.de/cms) deserve an honorable
 ///   mention here, as although they were keen to help with the implementation,
 ///   but their update procedure cannot be implemented so easily due to
 ///   technical limitations of the OpenRemise hardware.
 ///   <br><br>
 ///   \image{inline} html sine_wave.svg width=16px
-///   <b>Analog operation</b><br>
-///   <b>NO.</b>
+///   **Analog operation**<br>
+///   **NO.**
 ///   <br><br>
 /// </div>
 ///
 /// \section section_introduction_motivation Motivation
-/// \todo DCC-EX is great, but bound by hard- (and sw restrictions)
+/// \todo
+/// Compare OpenRemise to other open source projects like DCC-EX or Z21PG.
 ///
 /// <div class="section_buttons">
 /// | Next                      |
@@ -150,25 +154,53 @@
 
 /// \page page_getting_started Getting Started
 /// \details \tableofcontents
-/// \todo Where to get the hardware, how to install using the Flasher, YouTube?
+/// \todo
+/// Where to get the hardware, how to install using the Flasher, YouTube?
 ///
-/// \section section_getting_started_hardware Hardware
-/// Blar
+/// \section section_getting_started_buy_a_board Buy a board
+/// Kauf bei PCBWay
+///
+/// \subsection subsection_getting_started_or_manufacture ...or manufacture one
+/// Wenn ma keins kaufen will kann ma ja selbst eins machen.
+/// z.B. bei [PCBWay](https://www.pcbway.com/orderonline.aspx)
+///
+/// \section section_getting_started_install Install the Firmware
+/// Flasher
+///
+/// \section section_getting_started_wifi Connect to WiFi
+/// Captive portal thingy
 ///
 /// <div class="section_buttons">
 /// | Previous   | Next              |
 /// | :--------- | ----------------: |
-/// | \ref index | \ref page_faq |
+/// | \ref index | \ref page_faq     |
 /// </div>
 
 /// \page page_faq FAQ
 /// \details \tableofcontents
-/// \todo Where to get support? I guess GitHub issues mostly, maybe email?
+/// The \ref page_faq assumes that you already have a working OpenRemise system.
+/// For instructions on initial setup, please read the \ref page_getting_started
+/// guide.
 ///
-/// Wie verbindet man sich mit der Z21 App?
+/// \section section_faq_support Where to get support?
+/// Developers are only human and software may contain bugs. If you encounter
+/// any problems, please consider **in the following order** to either
+/// - open an issue at [GitHub](https://github.com/OpenRemise)
+/// - ask in one of the following forum threads
+///   - stummi
+///   - 1zu160
+///   - opendcc
+///   - rmweb
+///   - h0-modellbahnforum
+///   - kleinbahnsammle
+/// - or **after you have already exhausted the other options** contact me at
+///   vincent.hamp@openremise.at
 ///
-/// Wo bekommt ma den Stromstecker her?
+/// \section section_faq_z21_app How to connect to the Z21 app?
+/// Mit IP usw.
 ///
+/// \section section_faq_current_limit Which current limit is right for me?
+/// As a rule of thumb, we recommend the following settings.
 /// | Current [A] | Nominal size                                       |
 /// | ----------- | -------------------------------------------------- |
 /// | 0.5         | Service mode                                       |
@@ -176,14 +208,25 @@
 /// | 2.7         | [N scale](https://en.wikipedia.org/wiki/N_scale)   |
 /// | 4.1         | [H0 scale](https://en.wikipedia.org/wiki/HO_scale) |
 ///
+/// <div class="section_buttons">
+/// | Previous                  | Next                       |
+/// | :------------------------ | -------------------------: |
+/// | \ref page_getting_started | \ref page_getting_involved |
+/// </div>
+
+/// \page page_getting_involved Getting Involved
+/// \details \tableofcontents
+/// \todo
+/// Ka ob des a gute Idee is?
 ///
 /// <div class="section_buttons">
-/// | Previous                  | Next              |
-/// | :------------------------ | ----------------: |
-/// | \ref page_getting_started | \ref page_history |
+/// | Previous      | Next              |
+/// | :------------ | ----------------: |
+/// | \ref page_faq | \ref page_history |
 /// </div>
 
 /// \page page_history History
+/// \details \tableofcontents
 /// This project originally started back in 2021 as a decoder update device for
 /// my then (and still current) employer. I had already been working in the
 /// model railway industry since 2015 and had the feeling that the possibilities
@@ -191,9 +234,13 @@
 /// exploited. All decoder update devices that were on the market at that time
 /// only had USB.
 ///
-/// So I started working on a Bluetooth decoder update device in my free time,
-/// but the "Bluetooth part" didn't last long. One day I stumbled upon a demo of
-/// someone serving an HTML page from an
+/// So I started working on a Bluetooth decoder update device in my free time.
+///
+/// \todo
+/// Pictures of MSBlueZ here
+///
+/// The "Bluetooth part" didn't last long though. One day I stumbled upon a demo
+/// of someone serving an HTML page from an
 /// [ESP32](https://www.espressif.com/en/products/socs/esp32) over WiFi and I've
 /// obsessed over the idea ever since. Conveniently,
 /// [Espressif](https://www.espressif.com/) introduced an ESP32 with USB around
@@ -202,8 +249,6 @@
 /// [STM32](https://www.st.com/en/microcontrollers-microprocessors/stm32-32-bit-arm-cortex-mcus.html)
 /// soon became WiFi on an ESP32. At the time, I had not considered how much
 /// additional work my decision would entail...
-///
-/// \todo pictures of MSBlueZ here
 ///
 /// Suddenly I had to deal with HTTP requests and WebSockets, something I had no
 /// idea about. In that sense, this turned out to be a very educational side
@@ -214,7 +259,15 @@
 /// writing the [Frontend](https://openremise.at/Frontend), a combination that
 /// I've come to appreciate very much.
 ///
-/// \todo pictures of WULF here
+/// \todo
+/// Pictures of older WULFs here
+///
+/// However, the additional workload was not the only problem. The path to the
+/// final design of my H-bridge led via 3 different fully integrated ICs, each
+/// of which had its own problems...
+///
+/// \todo
+/// Pictures of stacked ESP32S3Board/DRV8328Shield
 ///
 /// When the project was in a somewhat presentable state, I showed it to my
 /// employer, who showed no interest at all. At this point it was clear that I
